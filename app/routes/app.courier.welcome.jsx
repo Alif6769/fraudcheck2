b71ece620b85687501b1d63b0d619580dfe66b42
Alif6769 <alif25755@gmail.com>
2026-03-16 21:29:01 +0600

    courier

---
+++ app/routes/app.courier.welcome.jsx
@@ -0,0 +1,8 @@
+export default function Welcome() {
+  return (
+    <s-stack gap="base">
+      <s-heading level="2">Welcome</s-heading>
+      <s-text>Setup forms will go here.</s-text>
+    </s-stack>
+  );
+}
