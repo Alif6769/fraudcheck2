e3759416ea567abf66296677914fe2a6b14bd37e
Alif6769 <alif25755@gmail.com>
2026-03-19 06:59:12 +0600

    pathao

--- app/hooks/useAuthenticatedFetch.js
+++ app/hooks/useAuthenticatedFetch.js
@@ -1,12 +1,15 @@
 // app/hooks/useAuthenticatedFetch.js
 import { useAppBridge } from "@shopify/app-bridge-react";
-import { authenticatedFetch } from "@shopify/app-bridge-utils";
+import { getSessionToken } from "@shopify/app-bridge-utils";
 
 export function useAuthenticatedFetch() {
   const app = useAppBridge();
-  const fetchFunction = authenticatedFetch(app);
   return async (url, options = {}) => {
-    const response = await fetchFunction(url, options);
-    return response;
+    const token = await getSessionToken(app);
+    const headers = {
+      ...options.headers,
+      Authorization: `Bearer ${token}`,
+    };
+    return fetch(url, { ...options, headers });
   };
 }
